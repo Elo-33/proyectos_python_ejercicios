82a04822a9f3bb45f77d8be626d8424185894df6
Elo-33 <elodie.fullola@gmail.com>
2025-11-25 18:58:27 +0100

    Instruccion del ejercicio

---
+++ adivinar.numero.py
@@ -0,0 +1,9 @@
+print('*** Juego de advivinar el numero ***')
+
+# 2. Juego de Adivinar el Número
+# Crea funciones para:
+#
+# Generar un número aleatorio
+# Verificar si el intento del usuario es correcto, mayor o menor
+# Contar los intentos realizados
+# Dar pistas al usuario
