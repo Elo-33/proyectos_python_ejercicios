8c9e0bf3b25298f0598a10d986bfa97b3bf91905
Elo-33 <elodie.fullola@gmail.com>
2025-11-28 19:22:51 +0100

    Empiezo de descripcion de funcion

--- adivinar.numero.py
+++ adivinar.numero.py
@@ -1,3 +1,5 @@
+from random import randint
+
 print('*** Juego de advivinar el numero ***')
 
 # 2. Juego de Adivinar el Número
@@ -7,3 +9,26 @@ print('*** Juego de advivinar el numero ***')
 # Verificar si el intento del usuario es correcto, mayor o menor
 # Contar los intentos realizados
 # Dar pistas al usuario
+
+def numero_aleatorio():
+    aleatorio = randint(1, 100)
+
+def verifica_numero(aleatorio):
+    if numero_jugador == aleatorio:
+        print('El numero es correcto')
+    elif numero_jugador > aleatorio:
+        print('Tu  numero es mas grande')
+    elif numero_jugador < aleatorio:
+        print('Tu numero es mas pequeño')
+    else:
+        print('Numero incorrecto')
+
+def contar_intentos():
+    intentos = 0
+
+def pista():
+    pass
+
+numero_jugador = int(input('Ingrese tu numero para adivinar (1-100): '))
+numero_aleatorio()
+verifica_numero()
