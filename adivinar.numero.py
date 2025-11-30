a5ecc9b208d2a001249a090ec1f717b88c444d85
Elo-33 <elodie.fullola@gmail.com>
2025-11-30 17:08:15 +0100

    Finalisacion de las funcion y contador

--- adivinar.numero.py
+++ adivinar.numero.py
@@ -5,30 +5,43 @@ print('*** Juego de advivinar el numero ***')
 # 2. Juego de Adivinar el Número
 # Crea funciones para:
 #
-# Generar un número aleatorio
+# Generar un número numero_a_buscar
 # Verificar si el intento del usuario es correcto, mayor o menor
 # Contar los intentos realizados
 # Dar pistas al usuario
 
 def numero_aleatorio():
-    aleatorio = randint(1, 100)
+    numero = randint(1, 100)
+    print(f'El numero aleatorio es {numero}')
+    return numero
 
-def verifica_numero(aleatorio):
-    if numero_jugador == aleatorio:
-        print('El numero es correcto')
-    elif numero_jugador > aleatorio:
-        print('Tu  numero es mas grande')
-    elif numero_jugador < aleatorio:
-        print('Tu numero es mas pequeño')
+def verifica_numero(numero_a_buscar, numero_jugador):
+    if numero_jugador == numero_a_buscar:
+        print('El numero es correcto has ganado')
+        return True
+    elif numero_jugador > numero_a_buscar:
+        print(f'Tu numero {numero_jugador} es mas grande')
+        return False
+    elif numero_jugador < numero_a_buscar:
+        print(f'Tu numero {numero_jugador} es mas pequeño')
+        return False
     else:
         print('Numero incorrecto')
+        return False
 
-def contar_intentos():
-    intentos = 0
+def contar_intentos(intentos):
+    return intentos + 1
 
-def pista():
-    pass
 
-numero_jugador = int(input('Ingrese tu numero para adivinar (1-100): '))
-numero_aleatorio()
-verifica_numero()
+numero_a_buscar = numero_aleatorio()
+salir = False
+intentos = 0
+
+while not salir:
+    numero_jugador = int(input('Ingrese tu numero para adivinar (1-100): '))
+    intentos = contar_intentos(intentos)
+
+    gano = verifica_numero(numero_a_buscar, numero_jugador)
+    if gano:
+        print(f'🎉 Lo lograste en {intentos} intentos !!!!')
+        salir = True
